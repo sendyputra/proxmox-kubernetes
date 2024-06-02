ba0b3862e726ce663bb301bad1e156b5e933b49e
Sendy Putra <sendyputra@gmail.com>
2024-06-02 21:34:12 +0700

    Refactor connection setup for kubespray provisioning
    Consolidate and optimize SSH connection setup for provisioner. This ensures more efficient configuration management during Kubernetes deployment. Refactor increases maintainability and reduces redundancy.

--- vm-kubespray-host.tf
+++ vm-kubespray-host.tf
@@ -76,6 +76,18 @@ module "kubespray_host" {
 resource "null_resource" "setup_kubespray" {
 
   count = var.create_kubespray_host ? 1 : 0
+  
+  # Establishes connection to be used by all
+  connection {
+    type         = "ssh"
+    user         = var.vm_user
+    private_key  = base64decode(var.ssh_private_key)
+    host         = module.kubespray_host.vm_list[0].ip0
+    port         = 22
+    bastion_host = var.bastion_ssh_ip
+    bastion_user = var.bastion_ssh_user
+    bastion_port = var.bastion_ssh_port
+  }
 
   provisioner "remote-exec" {
     inline = [
@@ -103,18 +115,6 @@ resource "null_resource" "setup_kubespray" {
       local.install_kubernetes_script_content
     ]
   }
-
-  connection {
-    type         = "ssh"
-    user         = var.vm_user
-    private_key  = base64decode(var.ssh_private_key)
-    host         = module.kubespray_host.vm_list[0].ip0
-    port         = 22
-    bastion_host = var.bastion_ssh_ip
-    bastion_user = var.bastion_ssh_user
-    bastion_port = var.bastion_ssh_port
-  }
-
   triggers = {
     always_run = timestamp()
   }
