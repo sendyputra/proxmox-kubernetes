a3bc874c3e12e964fdf1486d9fafca6882bba92f
Sendy Putra <sendyputra@gmail.com>
2024-06-03 10:12:10 +0700

    Add support for logging in Proxmox plugin
    Enable logging in the Proxmox provider configuration to facilitate debugging and troubleshooting. This change includes the addition of log-related settings to the provider configuration. The new logging feature will help in diagnosing issues and improving the overall maintainability of the plugin. No known issues are expected from this modification.

--- providers.tf
+++ providers.tf
@@ -20,4 +20,12 @@ provider "proxmox" {
   pm_tls_insecure     = var.pm_tls_insecure
   pm_parallel         = var.pm_parallel
   pm_timeout          = var.pm_timeout
+
+  pm_log_enable = true
+  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_debug      = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
 }
